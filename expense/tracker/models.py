cfe563b4a502ff315c9e04b14043bea3ae5232b5
shubham <shubhamkumargupta2@gmail.com>
2024-10-10 03:47:05 +0530

    Initial Commit

---
+++ expense/tracker/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class BaseModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4,editable=False, unique=True,primary_key=True)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now_add=True)
+    class Meta:
+        abstract = True
+
+class Transaction(BaseModel):
+    description = models.CharField(max_length=100)
+    amount = models.FloatField()
+    class Meta:
+        ordering = ('description',)
