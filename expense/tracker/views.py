22b1127acb1fb5c5b6dd44e5e3a4cbcb364af26e
shubham <shubhamkumargupta2@gmail.com>
2024-10-14 19:17:19 +0530

    Delete method completed

--- expense/tracker/views.py
+++ expense/tracker/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from tracker.models import Transaction
+from django.db.models import Sum
 
 # Create your views here.
 
@@ -20,5 +21,13 @@ def index(request):
             messages.info(request, "Description can not be None")
             return redirect('/')
         Transaction.objects.create(description=description,amount=amount)
-        context = {'transactions' : Transaction.objects.all()}
+    context = {'transactions' : Transaction.objects.all(),
+                'balance': Transaction.objects.all().aggregate(total_balance = Sum('amount'))['total_balance'] or 0,
+                'income': Transaction.objects.filter(amount__gte=0).aggregate(income = Sum('amount'))['income'] or 0,
+                'expense': Transaction.objects.filter(amount__lte=0).aggregate(expense = Sum('amount'))['expense'] or 0,
+                }
     return render(request, 'index.html',context)
+
+def deleteTransaction(request, uid):
+    Transaction.objects.get(uuid =uid).delete()
+    return redirect('/')
